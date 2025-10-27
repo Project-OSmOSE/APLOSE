d4317bb2b91310df37e3d2ac070514875f627684
Elodie MORIN <elodie.morin@ensta-bretagne.fr>
2025-10-27 10:24:34 +0100

    fix(test gql): allSpectrogramAnalysisForImport

--- backend/api/tests/schema/data/spectrogram_analysis/all_spectrogram_analysis_for_import.py
+++ backend/api/tests/schema/data/spectrogram_analysis/all_spectrogram_analysis_for_import.py
@@ -10,7 +10,7 @@ from graphene_django.utils.testing import GraphQLTestCase
 IMPORT_FIXTURES = settings.FIXTURE_DIRS[1] / "data" / "dataset" / "list_to_import"
 
 QUERY = """
-query ($datasetID: PK!) {
+query ($datasetID: ID!) {
     allSpectrogramAnalysisForImport(datasetId: $datasetID) {
         name
         path
