9f57273a9b0b3729375f4ae65ed6239b9caba6b6
Elodie MORIN <elodie.morin@ensta-bretagne.fr>
2026-02-06 08:14:04 +0100

    feat(annotation): Scenario B - generate from wav on GET request [APLOSE-376]

--- backend/api/views/zoom.py
+++ backend/api/views/zoom.py
@@ -2,11 +2,12 @@ from io import BytesIO
 
 import matplotlib.pyplot as plt
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 from osekit.core_api.audio_data import AudioData
 from osekit.core_api.spectro_data import SpectroData
 from rest_framework.decorators import action
 from rest_framework.viewsets import ViewSet
-from scipy.signal import ShortTimeFFT, spectrogram
+from scipy.signal import ShortTimeFFT
 from scipy.signal.windows import hamming
 
 from backend.api.models import Spectrogram, SpectrogramAnalysis
@@ -17,26 +18,30 @@ class ZoomViewSet(ViewSet):
 
     @action(
         detail=False,
-        url_path="zoom/(?P<level>[^/.]+)/(?P<tile>[^/.]+)",
+        url_path="analysis/(?P<analysis_id>[^/.]+)/spectrogram/(?P<spectrogram_id>[^/.]+)/zoom/(?P<zoom>[^/.]+)/tile/(?P<tile>[^/.]+)",
         url_name="zoom",
     )
-    def zoom(self, request, level=0, tile=0):
-        file: Spectrogram = Spectrogram.objects.filter(analysis__legacy=False).first()
-        analysis: SpectrogramAnalysis = file.analysis.filter(legacy=False).first()
-        audio_data: AudioData = file.get_spectro_data_for(analysis).audio_data
+    def zoom(self, request, analysis_id=None, spectrogram_id=None, zoom=0, tile=0):
+        print("view", analysis_id, spectrogram_id, zoom, tile)
+        zoom = int(zoom)
+        tile = int(tile)
 
-        zoom_level = pow(2, int(level))
+        file: Spectrogram = get_object_or_404(Spectrogram, pk=spectrogram_id)
+        analysis: SpectrogramAnalysis = get_object_or_404(
+            SpectrogramAnalysis, pk=analysis_id
+        )
+        audio_data: AudioData = file.get_audio_data_for(analysis)
 
-        audio_data = audio_data.split(zoom_level)[int(tile)]
+        zoom_level = pow(2, zoom)
+        audio_data = audio_data.split(zoom_level)[tile]
 
-        win_size = analysis.fft.window_size or 1_024
         overlap = analysis.fft.overlap or 0.95
-        hop = round(win_size * (1 - overlap))
+        hop = round(analysis.fft.window_size * (1 - overlap))
         spectro_data = SpectroData.from_audio_data(
             data=audio_data,
             fft=ShortTimeFFT(
-                win=hamming(win_size),
-                hop=hop // zoom_level,  # Improve temporal definition with zoom
+                win=hamming(analysis.fft.window_size),
+                hop=max(1, hop // zoom_level),  # Improve temporal definition with zoom
                 fs=analysis.fft.sampling_frequency,
                 scale_to="magnitude",
             ),
@@ -49,7 +54,14 @@ class ZoomViewSet(ViewSet):
 
         # Get the (plotted) image into memory file
         imgdata = BytesIO()
-        plt.savefig(imgdata, format="png", bbox_inches="tight", pad_inches=0)
+        plt.savefig(
+            imgdata,
+            transparent=False,
+            format="png",
+            bbox_inches="tight",
+            pad_inches=0,
+            dpi=72,
+        )
         imgdata.seek(0)  # rewind the data
 
         response = HttpResponse(content_type="image/png")
--- frontend/src/view/annotation-campaign/[campaignID]/phase/[phaseType]/spectrogram/[spectrogramID]/index.tsx
+++ frontend/src/view/annotation-campaign/[campaignID]/phase/[phaseType]/spectrogram/[spectrogramID]/index.tsx
@@ -7,10 +7,10 @@ import styles from './styles.module.scss';
 import { AnnotatorCanvasWindow } from '@/features/Annotator/Canvas';
 import { AnalysisSelect } from '@/features/Annotator/Analysis';
 import {
-  BrightnessSelect,
-  ColormapReverseButton,
-  ColormapSelect,
-  ContrastSelect,
+    BrightnessSelect,
+    ColormapReverseButton,
+    ColormapSelect,
+    ContrastSelect,
 } from '@/features/Annotator/VisualConfiguration';
 import { ZoomButtons } from '@/features/Annotator/Zoom';
 import { PointerInfo } from '@/features/Annotator/Pointer';
@@ -29,97 +29,98 @@ import { selectPosition } from '@/features/Annotator/Pointer';
 import { selectTaskIsEditionAuthorized } from '@/features/Annotator/selectors';
 
 export const AnnotatorPage: React.FC = () => {
-  // const { usedColormap: colormapClass } = useAnnotatorInput(); // TODO: check use: colormapClass was in div classes
-  const { campaignID } = useParams<AploseNavParams>();
+    const { campaignID } = useParams<AploseNavParams>();
 
-  const isEditionAuthorized = useAppSelector(selectTaskIsEditionAuthorized)
-  const {
-    spectrogram,
-    isFetching,
-    error,
-  } = useAnnotationTask({ refetchOnMountOrArgChange: true });
-  const audio = useAudio()
+    const isEditionAuthorized = useAppSelector(selectTaskIsEditionAuthorized)
+    const {
+        spectrogram,
+        isFetching,
+        error,
+    } = useAnnotationTask({ refetchOnMountOrArgChange: true });
+    const audio = useAudio()
 
-  useEffect(() => {
-    if (spectrogram?.audioPath) audio.setSource(spectrogram.audioPath)
-    else audio.clearSource()
+    useEffect(() => {
+        if (spectrogram?.audioPath) audio.setSource(spectrogram.audioPath)
+        else audio.clearSource()
 
-    return () => {
-      audio.clearSource() // TODO: check behavior when navigating between files
-    }
-  }, [ spectrogram ]);
+        return () => {
+            audio.clearSource() // TODO: check behavior when navigating between files
+        }
+    }, [ spectrogram ]);
 
-  const [ _previousCampaignID, _setPreviousCampaignID ] = useState<string>();
-  useEffect(() => {
-    if (_previousCampaignID !== campaignID) {
-      _setPreviousCampaignID(campaignID)
-      audio.setPlaybackRate(1)
-    }
-  }, [ campaignID ]);
+    const [ _previousCampaignID, _setPreviousCampaignID ] = useState<string>();
+    useEffect(() => {
+        if (_previousCampaignID !== campaignID) {
+            _setPreviousCampaignID(campaignID)
+            audio.setPlaybackRate(1)
+        }
+    }, [ campaignID ]);
 
-  const position = useAppSelector(selectPosition)
-  useEffect(() => {
-    if (position) { // Disable scroll
-      document.getElementsByTagName('html')[0].style.overflowY = 'hidden';
-    } else { // Enable scroll
-      document.getElementsByTagName('html')[0].style.overflowY = 'unset';
-    }
-  }, [ position ]);
+    const position = useAppSelector(selectPosition)
+    useEffect(() => {
+        if (position) { // Disable scroll
+            document.getElementsByTagName('html')[0].style.overflowY = 'hidden';
+        } else { // Enable scroll
+            document.getElementsByTagName('html')[0].style.overflowY = 'unset';
+        }
+    }, [ position ]);
 
-  if (isFetching) return <AnnotatorSkeleton children={ <IonSpinner/> }/>
-  if (error) return <AnnotatorSkeleton children={ <WarningText error={ error }/> }/>
-  if (!spectrogram) return <AnnotatorSkeleton><div></div></AnnotatorSkeleton>
+    if (isFetching) return <AnnotatorSkeleton children={ <IonSpinner/> }/>
+    if (error) return <AnnotatorSkeleton children={ <WarningText error={ error }/> }/>
+    if (!spectrogram) return <AnnotatorSkeleton>
+        <div></div>
+    </AnnotatorSkeleton>
 
-  return <AnnotatorSkeleton>
-    <div className={ styles.annotator }>
+    return <AnnotatorSkeleton>
+        <div className={ styles.annotator }>
 
-      <div className={ styles.spectrogramContainer }>
+            <div className={ styles.spectrogramContainer }>
 
-        <div className={ styles.spectrogramData }>
+                <div className={ styles.spectrogramData }>
 
-          <div className={ styles.spectrogramConfiguration }>
-            <AnalysisSelect/>
-            <div>
-              <ColormapSelect/>
-              <ColormapReverseButton/>
+                    <div className={ styles.spectrogramConfiguration }>
+                        <AnalysisSelect/>
+                        <div>
+                            <ColormapSelect/>
+                            <ColormapReverseButton/>
+                        </div>
+                        <BrightnessSelect/>
+                        <ContrastSelect/>
+                        <ZoomButtons/>
+                    </div>
+
+                    <PointerInfo/>
+                    <SpectrogramInfo/>
+                </div>
+
+                <AnnotatorCanvasWindow/>
+
+                <div className={ styles.spectrogramNavigation }>
+                    <div className={ styles.audioNavigation }>
+                        <PlayPauseButton/>
+                        <PlaybackRateSelect/>
+                    </div>
+                    <NavigationButtons/>
+                    <CurrentTime/>
+                </div>
             </div>
-            <BrightnessSelect/>
-            <ContrastSelect/>
-            <ZoomButtons/>
-          </div>
 
-          <PointerInfo/>
-          <SpectrogramInfo/>
+            <div className={ styles.blocContainer }>
+                { isEditionAuthorized && <Fragment>
+                    <FocusedAnnotationBloc/>
+                    <LabelsBloc/>
+                    <ConfidenceBloc/>
+                    <CommentBloc/>
+                    <AnnotationsBloc/>
+                </Fragment> }
+            </div>
+
+            <div className={ styles.downloadButtons }>
+                <AudioDownloadButton/>
+                <SpectrogramDownloadButton/>
+            </div>
         </div>
-
-        <AnnotatorCanvasWindow/>
-
-        <div className={ styles.spectrogramNavigation }>
-          <div className={ styles.audioNavigation }>
-            <PlayPauseButton/>
-            <PlaybackRateSelect/>
-          </div>
-          <NavigationButtons/>
-          <CurrentTime/>
-        </div>
-      </div>
-
-      <div className={ styles.blocContainer }>
-        { isEditionAuthorized && <Fragment>
-            <FocusedAnnotationBloc/>
-            <LabelsBloc/>
-            <ConfidenceBloc/>
-            <CommentBloc/>
-            <AnnotationsBloc/>
-        </Fragment> }
-      </div>
-
-      <div className={ styles.downloadButtons }>
-        <AudioDownloadButton/>
-        <SpectrogramDownloadButton/>
-      </div>
-    </div>
-  </AnnotatorSkeleton>
+    </AnnotatorSkeleton>
 }
 
 export default AnnotatorPage
