4e287b366f3ec6da8d4630872be19860f54af518
Elodie <150901577+ElodieENSTA@users.noreply.github.com>
2026-02-06 09:28:43 +0100

    fix(dataset): handle errors in dataset list available for import (#414)
    * fix(dataset): handle errors in dataset list available for import
    
    * pylint

--- frontend/src/components/ui/ImportRow.tsx
+++ frontend/src/components/ui/ImportRow.tsx
@@ -1,33 +1,66 @@
-import React, { ReactNode } from "react";
-import styles from "./ui.module.scss";
-import { IonButton, IonSpinner } from "@ionic/react";
+import React, { ReactNode, useCallback } from 'react';
+import styles from './ui.module.scss';
+import { IonButton, IonSpinner } from '@ionic/react';
+import { useToast } from '@/components/ui/toast.hook';
 
 export const ImportRow: React.FC<{
-  downloadedIcon: ReactNode;
-  downloadIcon: ReactNode;
-  isDownloaded?: boolean;
-  isLoading: boolean;
-  name: string;
-  path: string;
-  doImport: () => void;
-  children?: ReactNode
-}> = ({ isDownloaded, isLoading, downloadIcon, downloadedIcon, name, path, doImport, children }) => {
-  return <div className={ [ styles.importRow, isDownloaded ? styles.success : '' ].join(' ') }>
+    downloadedIcon: ReactNode;
+    downloadIcon: ReactNode;
+    isDownloaded?: boolean;
+    isLoading: boolean;
+    failedIcon?: ReactNode;
+    isFailed?: boolean | null;
+    errorStack?: string | null;
+    name: string;
+    path: string;
+    doImport: () => void;
+    children?: ReactNode
+}> = ({
+          isDownloaded,
+          isLoading,
+          downloadIcon,
+          downloadedIcon,
+          name,
+          path,
+          doImport,
+          children,
+          isFailed,
+          errorStack,
+          failedIcon,
+      }) => {
+    const toast = useToast();
 
-    { isDownloaded ?
-      <>{ downloadedIcon }</> :
-      isLoading ?
-        <IonSpinner/> :
-        <IonButton fill='clear' size='small' onClick={ doImport }>
-          { downloadIcon }
-        </IonButton>
-    }
+    const copyStack = useCallback(async () => {
+        if (!errorStack) return
+        await navigator.clipboard.writeText(errorStack)
+        toast.presentSuccess(`Error stacktrace has been copied into the clipboard`)
+    }, [ errorStack ])
 
-    <span className={ isDownloaded ? 'disabled' : '' }>
+    return <div className={ [
+        styles.importRow,
+        isDownloaded ? styles.success : '',
+        isFailed ? styles.error : '',
+    ].join(' ') }>
+
+        { isDownloaded ?
+            <>{ downloadedIcon }</> :
+            isLoading ?
+                <IonSpinner/> :
+                isFailed ?
+                    <IonButton fill="clear" size="small" color='danger' onClick={ copyStack }>
+                        { failedIcon }
+                    </IonButton>
+                    :
+                    <IonButton fill="clear" size="small" onClick={ doImport }>
+                        { downloadIcon }
+                    </IonButton>
+        }
+
+        <span className={ isDownloaded ? 'disabled' : '' }>
       <b>{ name }</b>
       <p>{ path }</p>
     </span>
 
-    { children }
-  </div>
+        { children }
+    </div>
 }
