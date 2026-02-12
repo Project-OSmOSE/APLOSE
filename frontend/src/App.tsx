7dda44d4f15af70b3706b7d8b70eb9c4160e1304
Elodie MORIN <elodie.morin@ensta-bretagne.fr>
2026-02-12 09:29:42 +0100

    fix(frontend): infinite redirection loop when trying to reach an unknown route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from 'react';
+import React, { lazy, Suspense, useEffect } from 'react';
 import { BrowserRouter, Navigate, Route, Routes, useLocation } from 'react-router-dom';
 
 import './css/bootstrap-4.1.3.min.css';
@@ -10,6 +10,12 @@ import './css/app.css';
 import { IonApp, setupIonicReact } from '@ionic/react';
 
 import { StoreProvider, useAppSelector } from '@/features/App';
+import { useLoadEventService } from '@/features/UX/Events';
+import { AlertProvider } from '@/components/ui/Alert';
+import { selectIsConnected } from '@/features/Auth';
+import { ReactFlowProvider } from '@xyflow/react';
+import { selectCurrentUser } from '@/api';
+import { AudioProvider } from '@/features/Audio';
 
 const Home = lazy(() => import('./view/home/Home'));
 const Login = lazy(() => import('./view/auth/Login'));
@@ -31,92 +37,92 @@ const AnnotatorPage = lazy(() => import('./view/annotation-campaign/[campaignID]
 
 const AploseSkeleton = lazy(() => import('./components/layout/Skeleton'));
 
-import { useLoadEventService } from '@/features/UX/Events';
-import { AlertProvider } from '@/components/ui/Alert';
-import { selectIsConnected } from '@/features/Auth';
-import { ReactFlowProvider } from '@xyflow/react';
-import { selectCurrentUser } from '@/api';
-import { AudioProvider } from '@/features/Audio';
-
 
 setupIonicReact({
-  mode: 'md',
-  spinner: 'crescent',
+    mode: 'md',
+    spinner: 'crescent',
 });
 
 export const App: React.FC = () => (
-  <StoreProvider>
-    <IonApp>
-      <BrowserRouter basename="/app/">
-        <AudioProvider>
-          <AlertProvider>
-            <ReactFlowProvider>
-              <AppContent/>
-            </ReactFlowProvider>
-          </AlertProvider>
-        </AudioProvider>
-      </BrowserRouter>
-    </IonApp>
-  </StoreProvider>
+    <StoreProvider>
+        <IonApp>
+            <BrowserRouter basename="/app/">
+                <AudioProvider>
+                    <AlertProvider>
+                        <ReactFlowProvider>
+                            <AppContent/>
+                        </ReactFlowProvider>
+                    </AlertProvider>
+                </AudioProvider>
+            </BrowserRouter>
+        </IonApp>
+    </StoreProvider>
 )
 
 const AppContent: React.FC = () => {
-  useLoadEventService();
+    useLoadEventService();
 
-  const isConnected = useAppSelector(selectIsConnected)
-  const currentUser = useAppSelector(selectCurrentUser)
+    const isConnected = useAppSelector(selectIsConnected)
+    const currentUser = useAppSelector(selectCurrentUser)
 
-  const from = useLocation()
+    const from = useLocation()
 
-  return (
-    <Routes>
+    useEffect(() => {
+        console.log('isConnected', isConnected)
+    }, [ isConnected ]);
 
-      <Route index element={ <Suspense><Home/></Suspense> }/>
+    return (
+        <Routes>
 
-      <Route path="login" element={ <Suspense><Login/></Suspense> }/>
+            <Route index element={ <Suspense><Home/></Suspense> }/>
 
-      { isConnected && <Route element={ <Suspense><AploseSkeleton/></Suspense> }>
+            <Route path="login" element={ <Suspense><Login/></Suspense> }/>
 
-          <Route path="dataset">
-              <Route index element={ <Suspense><DatasetList/></Suspense> }/>
-              <Route path=":datasetID" element={ <Suspense><DatasetDetail/></Suspense> }/>
-          </Route>
+            { isConnected && <Route element={ <Suspense><AploseSkeleton/></Suspense> }>
 
-          <Route path="annotation-campaign">
-              <Route index element={ <Suspense><AnnotationCampaignList/></Suspense> }/>
-              <Route path="new" element={ <Suspense><NewAnnotationCampaign/></Suspense> }/>
-              <Route path=":campaignID">
-                  <Route element={ <Suspense><AnnotationCampaignDetail/></Suspense> }>
-                      <Route index element={ <Suspense><AnnotationCampaignInfo/></Suspense> }/>
-                      <Route path="phase/:phaseType" element={ <Suspense><AnnotationCampaignPhaseDetail/></Suspense> }/>
-                  </Route>
-                  <Route path="phase/:phaseType">
-                      <Route path="edit-annotators" element={ <Suspense><EditAnnotators/></Suspense> }/>
-                      <Route path="import-annotations" element={ <Suspense><ImportAnnotations/></Suspense> }/>
+                <Route path="dataset">
+                    <Route index element={ <Suspense><DatasetList/></Suspense> }/>
+                    <Route path=":datasetID" element={ <Suspense><DatasetDetail/></Suspense> }/>
+                </Route>
 
-                      <Route path="spectrogram/:spectrogramID" element={ <Suspense><AnnotatorPage/></Suspense> }/>
-                  </Route>
-              </Route>
-          </Route>
+                <Route path="annotation-campaign">
+                    <Route index element={ <Suspense><AnnotationCampaignList/></Suspense> }/>
+                    <Route path="new" element={ <Suspense><NewAnnotationCampaign/></Suspense> }/>
+                    <Route path=":campaignID">
+                        <Route element={ <Suspense><AnnotationCampaignDetail/></Suspense> }>
+                            <Route index element={ <Suspense><AnnotationCampaignInfo/></Suspense> }/>
+                            <Route path="phase/:phaseType"
+                                   element={ <Suspense><AnnotationCampaignPhaseDetail/></Suspense> }/>
+                        </Route>
+                        <Route path="phase/:phaseType">
+                            <Route path="edit-annotators" element={ <Suspense><EditAnnotators/></Suspense> }/>
+                            <Route path="import-annotations" element={ <Suspense><ImportAnnotations/></Suspense> }/>
 
-          <Route path="account" element={ <Suspense><Account/></Suspense> }/>
-
-        { currentUser?.isSuperuser &&
-            <Route path="admin">
-                <Route path="sql" element={ <Suspense><SqlQuery/></Suspense> }/>
-                <Route path="ontology" element={ <Suspense><OntologyPage/></Suspense> }>
-                    <Route path=":type" element={ <Suspense><OntologyTab/></Suspense> }>
-                        <Route path=":id" element={ <Suspense><OntologyPanel/></Suspense> }/>
+                            <Route path="spectrogram/:spectrogramID" element={ <Suspense><AnnotatorPage/></Suspense> }/>
+                        </Route>
                     </Route>
                 </Route>
+
+                <Route path="account" element={ <Suspense><Account/></Suspense> }/>
+
+                { currentUser?.isSuperuser &&
+                    <Route path="admin">
+                        <Route path="sql" element={ <Suspense><SqlQuery/></Suspense> }/>
+                        <Route path="ontology" element={ <Suspense><OntologyPage/></Suspense> }>
+                            <Route path=":type" element={ <Suspense><OntologyTab/></Suspense> }>
+                                <Route path=":id" element={ <Suspense><OntologyPanel/></Suspense> }/>
+                            </Route>
+                        </Route>
+                    </Route> }
+
+                <Route path="" element={ <Navigate to="/annotation-campaign" replace/> }/>
             </Route> }
 
-          <Route path="" element={ <Navigate to="/annotation-campaign" replace state={ { from } }/> }/>
-      </Route> }
-
-      <Route path="*" element={ <Navigate to="/login" replace state={ { from } }/> }/>
-    </Routes>
-  )
+            { isConnected ?
+                <Route path="*" element={ <Navigate to="/annotation-campaign" replace/> }/> :
+                <Route path="*" element={ <Navigate to="/login" replace state={ { from } }/> }/> }
+        </Routes>
+    )
 }
 
 export default App;
