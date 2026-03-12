b1de18ff32781afa1a00c646992e2e996c1e921b
Elodie MORIN <elodie.morin@ensta-bretagne.fr>
2026-03-12 10:04:57 +0100

    fix 'folder'

--- backend/utils/osekit_replace.py
+++ backend/utils/osekit_replace.py
@@ -232,6 +232,6 @@ class OSEkitDataset:
                 }
 
             return OSEkitDataset(
-                folder=Path(make_absolute_server(make_path_relative(d["folder"]))),
+                folder=Path(make_absolute_server(make_path_relative(base_path))),
                 datasets=datasets,
             )
