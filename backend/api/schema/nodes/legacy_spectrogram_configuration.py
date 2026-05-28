a5587fab33c825989041fefca12ae399cef4b777
Elodie MORIN <elodie.morin@ensta-bretagne.fr>
2026-05-28 15:47:58 +0200

    fix(backend): lint

--- backend/api/schema/nodes/legacy_spectrogram_configuration.py
+++ backend/api/schema/nodes/legacy_spectrogram_configuration.py
@@ -1,11 +1,7 @@
 """LegacySpectrogramConfiguration schema"""
-import graphene_django_optimizer
 from django_extension.schema.types import ExtendedNode
-from graphene import String
 
 from backend.api.models import LegacySpectrogramConfiguration
-from .linear_scale import LinearScaleNode
-from .multi_linear_scale import MultiLinearScaleNode
 
 
 class LegacySpectrogramConfigurationNode(ExtendedNode):
