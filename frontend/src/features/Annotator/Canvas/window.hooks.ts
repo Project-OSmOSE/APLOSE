e19ca95b3dc5694e0d38e715f281e82430c6fee6
Elodie MORIN <elodie.morin@ensta-bretagne.fr>
2026-05-28 12:26:32 +0200

    fix(frontend): Box coordinates are wrong when the window is not zoomed at 100% [APLOSE-446]

--- frontend/src/features/Annotator/Canvas/window.hooks.ts
+++ frontend/src/features/Annotator/Canvas/window.hooks.ts
@@ -1,6 +1,6 @@
 import { useAppSelector } from '@/features/App';
 import { selectZoom } from '@/features/Annotator/Zoom';
-import { useMemo } from 'react';
+import { useLayoutEffect, useMemo, useState } from 'react';
 
 const SPECTRO_HEIGHT: number = 512;
 const SPECTRO_WIDTH: number = 1813;
@@ -8,23 +8,34 @@ export const Y_AXIS_WIDTH: number = 35;
 export const X_AXIS_HEIGHT: number = 30;
 
 
-const useWindowRatio = () =>
-  useMemo(() => window.devicePixelRatio * (1920 / (window.screen.width * window.devicePixelRatio)), [])
+const useWindowRatio = () => {
+    const [width, setWidth] = useState(window.innerWidth);
+    useLayoutEffect(() => {
+        function updateSize() {
+            setWidth(window.innerWidth)
+        }
+        window.addEventListener('resize', updateSize);
+        updateSize();
+        return () => window.removeEventListener('resize', updateSize);
+    }, []);
+
+    return useMemo(() => window.devicePixelRatio * (1920 / (width * window.devicePixelRatio)), [width])
+}
 
 export const useWindowContainerWidth = () => {
-  const ratio = useWindowRatio()
-  return useMemo(() => SPECTRO_WIDTH / ratio, [ ratio ])
+    const ratio = useWindowRatio()
+    return useMemo(() => SPECTRO_WIDTH / ratio, [ ratio ])
 }
 
 export const useWindowWidth = () => {
-  const zoom = useAppSelector(selectZoom)
-  const containerWidth = useWindowContainerWidth()
+    const zoom = useAppSelector(selectZoom)
+    const containerWidth = useWindowContainerWidth()
 
-  return useMemo(() => containerWidth * zoom, [ containerWidth, zoom ])
+    return useMemo(() => containerWidth * zoom, [ containerWidth, zoom ])
 }
 
 export const useWindowHeight = () => {
-  const ratio = useWindowRatio()
+    const ratio = useWindowRatio()
 
-  return useMemo(() => SPECTRO_HEIGHT / ratio, [ ratio ])
+    return useMemo(() => SPECTRO_HEIGHT / ratio, [ ratio ])
 }
