b05a2ff896422bb2aada8d5531774c580d8f4f5d
Elodie MORIN <elodie.morin@ensta-bretagne.fr>
2026-06-09 08:55:10 +0200

    feat(frontend.components): Input new types textarea & date

--- frontend/src/components/base/Input/Input.tsx
+++ frontend/src/components/base/Input/Input.tsx
@@ -2,11 +2,13 @@ import React, { type HTMLInputTypeAttribute } from 'react';
 import { Input as BaseInput, type InputProps as BaseInputProps } from '@base-ui/react'
 import styles from './Input.module.scss'
 
-export type InputProps = Omit<BaseInputProps, 'type'> & {
-    type: HTMLInputTypeAttribute & ('text' | 'url' | 'email')
+export type InputProps = Omit<BaseInputProps, 'type' | 'render'> & {
+    type: HTMLInputTypeAttribute & ('text' | 'url' | 'date' | 'email' | 'textarea')
 }
 
-export const Input: React.FC<InputProps> = React.memo(({ className, ...props }) => {
-    return <BaseInput className={ [ className, styles.Input ].join(' ') }
-                      { ...props }/>
-})
+export const Input: React.FC<InputProps> = React.memo(({ className, type, ...props }) => (
+    <BaseInput className={ [ className, styles.Input ].join(' ') }
+               render={ type === 'textarea' ? <textarea/> : undefined }
+               type={ type }
+               { ...props }/>
+))
