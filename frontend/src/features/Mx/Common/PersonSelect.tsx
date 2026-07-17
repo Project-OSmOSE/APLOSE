bb516130a7aae59c801e3f08dc059c5a974f329c
Elodie MORIN <elodie.morin@ensta-bretagne.fr>
2026-07-17 08:51:08 +0200

    feat(frontend.mx): add mx.common person select

---
+++ frontend/src/features/Mx/Common/PersonSelect.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { ComboboxSelect, type ComboboxSelectProps } from '@/components/base/Combobox'
+import { NewPersonDialog } from './NewPersonDialog'
+import * as API from './api'
+
+type Props =
+    Omit<ComboboxSelectProps<API.PersonFragment, API.CreatePersonMutationVariables['input']>, 'createDialog' | 'itemName' | 'inputKey'>
+
+export const PersonSelect: React.FC<Props> = ({ creatable, items, ...props }) => {
+    const { data: persons } = useQuery({ ...API.allPersonsQuery, enabled: !items })
+
+    return <ComboboxSelect itemName="person"
+                           itemToStringLabel={ item => `${ item.firstName } ${ item.lastName }` }
+                           itemToStringValue={ item => item.id }
+                           isItemEqualToValue={ (a, b) => a.id === b.id }
+                           creatable={ creatable }
+                           createDialog={ NewPersonDialog }
+                           items={ items || persons }
+                           inputKey="lastName"
+                           { ...props }/>
+}
