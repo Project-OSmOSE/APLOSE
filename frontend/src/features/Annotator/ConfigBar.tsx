3e0ff8d5062fbd6714c082bb635540c88cad5121
Elodie MORIN <elodie.morin@ensta-bretagne.fr>
2026-09-25 09:53:30 +0200

    fix(backend): metadatax update

--- frontend/src/features/Annotator/ConfigBar.tsx
+++ frontend/src/features/Annotator/ConfigBar.tsx
@@ -6,14 +6,14 @@ import {
 } from '@solar-icons/react';
 import { useLoaderData } from '@tanstack/react-router';
 import { useAnnotatorAnalysis } from '@/features/Annotator/Analysis';
-import { ButtonGroup, Dialog, Popover, Note } from '@/components/base';
+import { ButtonGroup, Dialog, Note, Popover } from '@/components/base';
 import { Zoom } from '@/features/Annotator/Zoom';
 import { usePointer } from '@/features/Annotator/Pointer';
 import { formatTime } from '@/service/function';
 import { AnalysisComponent } from '@/features/SpectrogramAnalysis';
 import { ImageSettings } from './ImageSettings';
 import { MxData } from "../Mx";
-import { VisualObservationFragment } from "@/features/AnnotationSpectrogram";
+import { cleanGqlList } from "@/api/utils.ts";
 
 export const ConfigBar: React.FC = () => {
     const {
@@ -29,37 +29,7 @@ export const ConfigBar: React.FC = () => {
     // Pointer
     const pointer = usePointer()
 
-    const obs = useMemo(() => [
-            {
-                source: {
-                    displayName: 'Stripped dolphin',
-                },
-                otherHumanActivityPresence: true,
-                youngPresence: false,
-                behaviors: [
-                    { name: 'eat' },
-                    { name: 'sleep' },
-                    { name: 'rest' },
-                    { name: 'mate' }
-                ],
-                reactionsToBoat: [
-                    { name: 'avoid the boat' },
-                ],
-                startDatetime: spectrogram.start,
-                endDatetime: spectrogram.start,
-                additionalInformation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In semper facilisis tortor nec venenatis. Nulla a feugiat lectus. Duis tortor enim, hendrerit quis lacus in, aliquet scelerisque arcu. Pellentesque consectetur faucibus aliquam. In a pretium augue. Phasellus non sem lacinia, convallis neque non, vulputate nibh. In blandit cursus libero, sed gravida augue rhoncus in. Suspendisse potenti. Sed posuere massa et urna dapibus, sit amet gravida turpis placerat. Aliquam vulputate gravida fringilla. Curabitur sit amet sapien non metus gravida ornare. Nulla facilisi. Fusce id arcu quis purus venenatis lacinia. Aenean vehicula nisi vitae dolor accumsan tristique. Integer sapien magna, venenatis ut tincidunt non, maximus eu lacus. Maecenas eget varius nisi.\n' +
-                    '\n' +
-                    'In hac habitasse platea dictumst. Etiam viverra, risus mollis auctor dictum, lacus tellus lobortis nunc, id consequat nunc magna at lorem. Vestibulum massa libero, tempor egestas pulvinar sit amet, vestibulum nec nunc. Etiam tristique elit tellus, a imperdiet mi molestie ut. Duis lobortis magna quis eros sodales, id ultrices purus venenatis. Maecenas fringilla risus erat, ac ultrices nisi consectetur ac. Vivamus vel enim eu nunc eleifend porttitor. Suspendisse in dolor nulla. Vivamus porta imperdiet tellus. Donec sit amet auctor orci. Curabitur id molestie nibh. Ut vitae ultricies tortor, sed euismod elit.',
-                countMin: 10,
-                countMax: 50,
-                startDistanceMin: 10,
-                startDistanceMax: 100,
-                endDistanceMin: 1000,
-                endDistanceMax: null,
-            } satisfies VisualObservationFragment
-        ]
-        // || cleanGqlList(spectrogram.visualObservations)
-        , [ spectrogram ])
+    const obs = useMemo(() => cleanGqlList(spectrogram.visualObservations), [ spectrogram ])
 
     return <ButtonGroup spaceBetween>
 
